b88e591827213513c98d14ee7c9bce9cdb644a24
Warren Milburn <warren@munki.net>
2020-05-09 00:39:55 +0100

    No longer relevant and out of date.

--- compile_scripts/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-javac -d "./out/production/Javabot\ TOS" -classpath ./lib/JSX0.8.20.jar:./lib/gnu-regexp-1.1.3.jar:./lib/bsh-1.2b5.jar:. org/javabot/engine/*.java org/javabot/configuration/*.java org/javabot/security/*.java org/javabot/message/*.java org/javabot/channel/*.java org/javabot/task/*.java org/javabot/user/*.java org/javabot/util/*.java org/javabot/script/*.java
--- run_scripts/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -classpath ../lib/JSX0.8.20.jar:../lib/gnu-regexp-1.1.3.jar:../lib/bsh-1.2b5.jar:. org.javabot.gui.javabot
