9212716190ed40fcb063f757b5d54c2fb650cb01
Warren Milburn <warren@munki.net>
2020-05-08 12:54:20 +0100

    New Users class for a collection of User.

---
+++ src/org/javabot/user/Users.java
@@ -0,0 +1,27 @@
+package org.javabot.user;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+@XmlRootElement
+public class Users {
+
+    private ArrayList<User> users;
+
+    public Users() {
+        users = new ArrayList<>();
+    }
+
+    public ArrayList<User> getUsers() {
+        return this.users;
+    }
+
+    // XmLElementWrapper generates a wrapper element around XML representation
+    // XmlElement sets the name of the entities in collection
+    @XmlElement(name = "user")
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+}
