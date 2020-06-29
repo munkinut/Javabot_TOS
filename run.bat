e7e4ef51afb3d18c3d08f1f77129f1fd414819fe
Warren Milburn <warren@munki.net>
2020-06-29 10:22:47 +0100

    Constructed the necessary files to run JavaBot.

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
-java -Djava.util.logging.config.file=%CD%\config\javabot.logging.properties -Djavabot.properties.file=%CD%\config\javabot.properties -Djava.rmi.server.codebase=http://localhost:8000/ -Djava.security.policy=%CD%\client.policy org.javabot.JavaBot
+C:\Users\Warren\.jdks\adopt-openjdk-11.0.7\bin\java.exe --module-path target/libs --module Runner -Djava.util.logging
+.config.file=C:\Users\Warren\IdeaProjects\Javabot_TOS\config\javabot.logging.properties -Djavabot.properties.file=C:\Users\Warren\IdeaProjects\Javabot_TOS\con
+fig\javabot.properties -Djava.rmi.server.codebase=http://localhost:8000/ -Djava.security.policy=C:\Users\Warren\IdeaProjects\Javabot_TOS\client.policy -Dfile.
+encoding=UTF-8 -m Runner/org.javabot.JavaBot
